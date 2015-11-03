fc68971c85b819bd29635a1565907e83c941e673
MattBrown <litteriver1337@gmail.com>
2015-11-03 11:22:45 -0500

    added TEST!!!

---
+++ src/test/com/theironyard/MainTest.java
@@ -0,0 +1,36 @@
+package com.theironyard;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MattBrown on 11/3/15.
+ */
+public class MainTest {
+    public Connection startConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./test");
+        Main.createTables(conn);
+        return conn;
+    }
+    public void endConnection(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("DROP TABLE users");
+        stmt.execute("DROP TABLE messages");
+        conn.close();
+    }
+    @Test
+    public void testUser() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertUser(conn, "Alice", "");
+        User user = Main.selectUser(conn, "Alice");
+        endConnection(conn);
+
+        assertTrue(user != null);
+    }
+}
